ce210a158cea8eaaf52bf79be8a8e816490dfab8
ImranLokmic <imranlokmic99@gmail.com>
2020-10-31 13:30:17 +0100

    ModalMenu animations reworked. Close button needs fixing, svg maps next.

--- scripts/static.js
+++ scripts/static.js
@@ -77,9 +77,40 @@ submenuitemfour.onmouseout = function(){
 var modalMenu = document.getElementById("hamModal");
 var btnMenu = document.getElementById("hamBtn");
 var spanMenu = document.getElementsByClassName("closeMenu")[0];
+var menuIconOne = document.getElementById("modalItemOne");
+var menuIconTwo = document.getElementById("modalItemTwo");
+var menuIconThree = document.getElementById("modalItemThree");
+var menuIconFour = document.getElementById("modalItemFour");
+var menuIconFive = document.getElementById("modalItemFive");
+var hr = document.getElementsByClassName("menuHr");
 
 btnMenu.onclick = function() {
   modalMenu.style.display = "block";
+
+  menuIconOne.style.transform = "translate(0,0)";
+  menuIconOne.style.opacity = "1";
+
+  menuIconTwo.style.transform = "translate(0,0)";
+  menuIconTwo.style.opacity = "1";
+
+  subone.style.opacity = "0";
+
+  menuIconThree.style.transform = "translate(0,0)";
+  menuIconThree.style.opacity = "1";
+
+  menuIconFour.style.transform = "translate(0,0)";
+  menuIconFour.style.opacity = "1";
+
+  menuIconFive.style.transform = "translate(0,0)";
+  menuIconFive.style.opacity = "1";
+
+  subtwo.style.opacity = "0";
+  subtwo.style.transform = "translate(0px,0px)"
+
+  for (var i = 0; i < hr.length; i++){
+    hr[i].style.transform = "translate(0,0)";
+    hr[i].style.opacity = "1";
+  }
 }
 spanMenu.onclick = function() {
   modalMenu.style.display = "none";
@@ -95,42 +126,82 @@ window.addEventListener("click", function(event) {
 
 
 //SUBMENU VARS
-var submodalone = document.getElementById("subModalOne");
-var submodaltwo = document.getElementById("subModalTwo");
-var subcloseone = document.getElementById("subCloseOne");
-var subclosetwo = document.getElementById("subCloseTwo");
+var subone = document.getElementById("submodal-content-one");
+var subtwo = document.getElementById("submodal-content-two");
 
 
 //SUBMENU ONE TRIGGERS
 function submenuone() {
-  submodalone.style.display = "inline-block";
-  modalMenu.style.display = "none";
-}
-window.addEventListener("click", function(event) {
-  if (event.target == submodalone) {
-    submodalone.style.display = "none";
+    //submodalone.style.display = "inline-block";
+    menuIconOne.style.transform = "translate(0,-300px)";
+    menuIconOne.style.opacity = "0";
+    menuIconOne.style.transition = "transform ease-in,transform 2s, opacity 3s";
+    menuIconOne.style.overflow = "hidden";
+
+    menuIconTwo.style.transform = "translate(0,-130px)";
+    menuIconTwo.style.transition = "transform ease-in,transform 2s";
+    menuIconTwo.style.overflow = "hidden";
+
+    menuIconThree.style.transform = "translate(-600px,0px)";
+    menuIconThree.style.opacity = "0";
+    menuIconThree.style.transition = "transform ease-in,transform 2s, opacity 3s";
+    menuIconThree.style.overflow = "hidden";
+
+    menuIconFour.style.transform = "translate(0px,300px)";
+    menuIconFour.style.opacity = "0";
+    menuIconFour.style.transition = "transform ease-in,transform 2s, opacity 3s";
+    menuIconFour.style.overflow = "hidden";
+
+    menuIconFive.style.transform = "translate(0px,200px)";
+    menuIconFive.style.opacity = "0";
+    menuIconFive.style.transition = "transform ease-in,transform 2s, opacity 3s";
+    menuIconFive.style.overflow = "hidden";
+
+    for (var i = 0; i < hr.length; i++){
+      hr[i].style.transform = "translate(500px,0)";
+      hr[i].style.opacity ="0";
+      hr[i].style.transition = "transform ease-in,transform 2s, opacity 3s";
+    }
+
+    subone.style.opacity = "1";
+    subone.style.transition = "opacity 4s";
+
   }
-});
 
-subcloseone.onclick = function() {
-  submodalone.style.display = "none";
-  modalMenu.style.display = "inline-block";
-}
+  function submenutwo() {
+      //submodalone.style.display = "inline-block";
+      menuIconOne.style.transform = "translate(0,-300px)";
+      menuIconOne.style.opacity = "0";
+      menuIconOne.style.transition = "transform ease-in,transform 2s, opacity 3s";
+      menuIconOne.style.overflow = "hidden";
 
+      menuIconTwo.style.transform = "translate(0,-400px)";
+      menuIconTwo.style.opacity = "0";
+      menuIconTwo.style.transition = "transform ease-in,transform 2s, opacity 3s";
+      menuIconTwo.style.overflow = "hidden";
 
-//SUBMENU TWO TRIGGERS
-function submenutwo() {
-  submodaltwo.style.display = "inline-block";
-  modalMenu.style.display = "none";
-}
+      menuIconThree.style.transform = "translate(-600px,0px)";
+      menuIconThree.style.opacity = "0";
+      menuIconThree.style.transition = "transform ease-in,transform 2s, opacity 3s";
+      menuIconThree.style.overflow = "hidden";
 
-window.addEventListener("click", function(event) {
-  if (event.target == submodaltwo) {
-    submodaltwo.style.display = "none";
-  }
-});
+      menuIconFour.style.transform = "translate(0px,300px)";
+      menuIconFour.style.opacity = "0";
+      menuIconFour.style.transition = "transform ease-in,transform 2s, opacity 3s";
+      menuIconFour.style.overflow = "hidden";
 
-subclosetwo.onclick = function() {
-  submodaltwo.style.display = "none";
-  modalMenu.style.display = "inline-block";
-}
+      menuIconFive.style.transform = "translate(0px,-470px)";
+      menuIconFive.style.transition = "transform ease-in,transform 2s";
+      menuIconFive.style.overflow = "hidden";
+
+      for (var i = 0; i < hr.length; i++){
+        hr[i].style.transform = "translate(500px,0)";
+        hr[i].style.opacity ="0";
+        hr[i].style.transition = "transform ease-in,transform 2s, opacity 3s";
+      }
+
+      subtwo.style.transform = "translate(0px,-430px)"
+      subtwo.style.opacity = "1";
+      subtwo.style.transition = "transform ease-in,transform 2s,opacity 4s";
+
+    }
